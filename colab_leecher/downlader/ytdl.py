ae67d037a127dbf9c551775bc499862b622352a6
alwaysmaysix <104639059+alwaysmaysix@users.noreply.github.com>
2024-08-30 04:11:22 +0200

    Update ytdl.py

--- colab_leecher/downlader/ytdl.py
+++ colab_leecher/downlader/ytdl.py
@@ -96,7 +96,7 @@ def YouTubeDL(url):
             logging.info(d)
 
     ydl_opts = {
-        "format": "best",
+        "format": "worst",
         "allow_multiple_video_streams": True,
         "allow_multiple_audio_streams": True,
         "writethumbnail": True,
